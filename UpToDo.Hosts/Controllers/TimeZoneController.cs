70ef20520a2209ef4be2233213e32e1b8be60d07
vladislav.g <sdive1337@gmail.com>
2025-05-30 14:45:36 +0300

    к предыдущему коммиту

--- UpToDo.Hosts/Controllers/TimeZoneController.cs
+++ UpToDo.Hosts/Controllers/TimeZoneController.cs
@@ -1,6 +1,24 @@
-namespace UpToDo.Hosts.Controllers;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UpToDo.Application.TimeZones.Queries;
+using UpToDo.Contracts.TimeZones.Responses;
 
-public class TimeZoneController
+namespace UpToDo.Hosts.Controllers
 {
-    
+    [Route("time-zone")]
+    [ApiController]
+    public class TimeZoneController(IMediator mediator) : ControllerBase
+    {
+        /// <summary>
+        /// Получить все таймзоны.
+        /// </summary>
+        [HttpGet]
+        public async Task<ActionResult<List<TimeZoneItemResponse>>> GetAll()
+        {
+            var query = new GetAllTimeZoneItemsQuery();
+            var response = await mediator.Send(query);
+            return Ok(response);
+        }
+    }
 }
